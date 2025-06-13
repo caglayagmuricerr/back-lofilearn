c108a71c8d93c675a8463af0e36cd569b9fc6364
caglayagmuricerr <caglayagmuricer@gmail.com>
2025-06-13 21:09:00 +0300

    improve lobby management and user disconnection

--- socketio.js
+++ socketio.js
@@ -25,34 +25,64 @@ module.exports = (io) => {
   });
 
   io.on("connection", (socket) => {
-    socket.on("join-lobby", ({ inviteCode }) => {
-      const name = socket.user.name;
-      console.log(`${name} is trying to join lobby: ${inviteCode}`);
-      socket.join(inviteCode);
-      socket.data.username = name;
+    const name = socket.user.name;
+    const role = socket.user.role;
 
-      if (!lobbies[inviteCode]) lobbies[inviteCode] = [];
-      lobbies[inviteCode].push(name);
+    console.log("User connected:", name, role);
+
+    socket.data.username = name;
+    socket.data.role = role;
+
+    socket.on("join-lobby", ({ inviteCode }) => {
+      console.log(`${name} is trying to join lobby: ${inviteCode}`);
+
+      socket.join(inviteCode);
+      socket.data.inviteCode = inviteCode;
+
+      if (!lobbies[inviteCode]) {
+        lobbies[inviteCode] = [];
+        console.log(`Created new lobby: ${inviteCode}`);
+      }
+      lobbies[inviteCode] = lobbies[inviteCode].filter(
+        (player) => player.name !== name
+      );
+
+      lobbies[inviteCode].push({ name, role });
+      console.log(`Lobby ${inviteCode} now has players:`, lobbies[inviteCode]);
 
       // emit updated player list to the lobby
       io.to(inviteCode).emit("lobby-update", {
         players: lobbies[inviteCode],
         message: `${name} joined the lobby.`,
       });
+      console.log(`Emitted lobby-update to ${inviteCode}`);
     });
 
     socket.on("disconnecting", () => {
+      console.log(`User ${socket.data.username} is disconnecting`);
       for (const inviteCode of socket.rooms) {
         if (lobbies[inviteCode]) {
-          lobbies[inviteCode] = lobbies[inviteCode].filter(
-            (n) => n !== socket.data.username
+          console.log(
+            `Removing ${socket.data.username} from lobby ${inviteCode}`
           );
+          lobbies[inviteCode] = lobbies[inviteCode].filter(
+            (player) => player.name !== socket.data.username
+          );
+
           io.to(inviteCode).emit("lobby-update", {
             players: lobbies[inviteCode],
             message: `${socket.data.username} left the lobby.`,
           });
+          // if the lobby is empty, remove it
+          if (lobbies[inviteCode].length === 0) {
+            delete lobbies[inviteCode];
+          }
         }
       }
+    });
+
+    socket.on("disconnect", () => {
+      console.log("User disconnected:", socket.user?.name);
     });
 
     // TODO: Handle quiz start, question submission, score updates, etc.
